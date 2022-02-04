1bd0ea48436dedf0b4395be7f6a3f274dcc6193a
fahmi54321 <fahmiabdulaziz44@gmail.com>
2022-02-04 20:54:48 +0700

    add method untuk convert coordinat ke address

--- lib/helpers/location_helper.dart
+++ lib/helpers/location_helper.dart
@@ -1,4 +1,7 @@
-//todo 1 (next location_input)
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
 
 const GOOGLE_API_KEY = 'AIzaSyDI4X-fug0yawUaQ0I4xeNUXmaSRxlb5B8';
 
@@ -9,4 +12,84 @@ class LocationHelper {
   }) {
     return 'https://maps.googleapis.com/maps/api/staticmap?center=&$latitude,$longitude&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:C%7C$latitude,$longitude&key=$GOOGLE_API_KEY';
   }
+
+  //todo 1 (finish)
+  static Future<String> getPlaceAddress(double lat, double lng) async{
+    final url = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=$lat,$lng&key=$GOOGLE_API_KEY';
+    final response = await http.get(Uri.parse(url));
+    return jsonDecode(response.body)['results'][0]['formatted_address'];
+  }
+
 }
+
+/* hasil response geocoding api untuk mencari address berdasarkan latlng
+{
+   "results" : [
+      {
+         "address_components" : [
+            {
+               "long_name" : "277",
+               "short_name" : "277",
+               "types" : [ "street_number" ]
+            },
+            {
+               "long_name" : "Bedford Avenue",
+               "short_name" : "Bedford Ave",
+               "types" : [ "route" ]
+            },
+            {
+               "long_name" : "Williamsburg",
+               "short_name" : "Williamsburg",
+               "types" : [ "neighborhood", "political" ]
+            },
+            {
+               "long_name" : "Brooklyn",
+               "short_name" : "Brooklyn",
+               "types" : [ "political", "sublocality", "sublocality_level_1" ]
+            },
+            {
+               "long_name" : "Kings County",
+               "short_name" : "Kings County",
+               "types" : [ "administrative_area_level_2", "political" ]
+            },
+            {
+               "long_name" : "New York",
+               "short_name" : "NY",
+               "types" : [ "administrative_area_level_1", "political" ]
+            },
+            {
+               "long_name" : "United States",
+               "short_name" : "US",
+               "types" : [ "country", "political" ]
+            },
+            {
+               "long_name" : "11211",
+               "short_name" : "11211",
+               "types" : [ "postal_code" ]
+            }
+         ],
+         "formatted_address" : "277 Bedford Ave, Brooklyn, NY 11211, USA",
+         "geometry" : {
+            "location" : {
+               "lat" : 40.7142205,
+               "lng" : -73.9612903
+            },
+            "location_type" : "ROOFTOP",
+            "viewport" : {
+               "northeast" : {
+                  "lat" : 40.71556948029149,
+                  "lng" : -73.95994131970849
+               },
+               "southwest" : {
+                  "lat" : 40.7128715197085,
+                  "lng" : -73.9626392802915
+               }
+            }
+         },
+         "place_id" : "ChIJd8BlQ2BZwokRAFUEcm_qrcA",
+         "types" : [ "street_address" ]
+      }
+   ],
+   "status" : "OK"
+}
+ */
